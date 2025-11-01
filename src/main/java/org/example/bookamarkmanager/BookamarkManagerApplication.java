0195ea234b4345499412ab4a6ad4c01beb169477
Asus <agavindu123@gmail.com>
2025-11-01 20:45:03 +0530

    first commit

---
+++ src/main/java/org/example/bookamarkmanager/BookamarkManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookamarkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookamarkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookamarkManagerApplication.class, args);
+	}
+
+}
